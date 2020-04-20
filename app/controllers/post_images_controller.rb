7218f49ef190b640a31aaf73af4e9a57d39861b5
名前 <>
2020-04-20 14:11:30 +0900

    [Finish]5-4

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -5,22 +5,26 @@ class PostImagesController < ApplicationController
 	end
 
 	def create
-		@post_image=PostImage.new(post_image_params)#モデルは大文字、インスタンスはあんだーばー
-		@post_image.user_id=current_user.id
-		@post_image.save
-		redirect_to post_images_path
+	    @post_image = PostImage.new(post_image_params)
+	    @post_image.user_id = current_user.id
+	    if @post_image.save
+	    	redirect_to post_images_path
+	    else
+	    	render :new
+	    end
 	end
 
 	def index
-		@post_images=PostImage.all
+		@post_images = PostImage.page(params[:page]).reverse_order
 	end
 
 	def show
-		@post_image=PostImage.find(params[:id])
+	    @post_image = PostImage.find(params[:id])
+	    @post_comment = PostComment.new
 	end
 
 	private
-	def post_image_params
-		params.require(:post_image).permit(:shop_name, :image, :caption)
-	end
+    def post_image_params
+        params.require(:post_image).permit(:shop_name, :image, :caption)
+    end
 end
